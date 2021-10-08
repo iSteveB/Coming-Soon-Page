e31f47f07733a58f83c06426889f7aa0716817af
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2021-10-08 21:21:52 +0200

    Fin du JS pour la vérification du mail

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 const input = document.querySelector("input");
-const button = document.querySelector("button");
+const form = document.getElementById("mailForm");
 
 let email = "";
 
-
 const emailChecker = (value) => {
 
-    input.addEventListener('input', (e) => {
-        email = e.target.value;
-        console.log(email);
-    });
-            if(!value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-
-                document.querySelector("input").classList.add("error");
-                document.querySelector("form > p").classList.remove ("hidden");
-                email = null;
-            console.log('email incorrect');
-
-            } else {
-                document.querySelector("input").classList.remove("error");
-                document.querySelector("form > p").classList.add("hidden");
-                email = value;
-                console.log("email correct !");
-            };
+    if(!value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        
+        document.querySelector("input").classList.add("error");
+        document.querySelector("form > p").classList.remove ("hidden");
+        email = null;
+        
+    } else {
+        
+        document.querySelector("input").classList.remove("error");
+        document.querySelector("form > p").classList.add("hidden");
+        email = value;
+    };
 };
 
-button.addEventListener('click', (e)=>{
-    e.preventDefault();
+input.addEventListener('input', (e) => {
+    email = e.target.value;
+    console.log(email);
+});
 
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
     emailChecker(email);
-})
+});
